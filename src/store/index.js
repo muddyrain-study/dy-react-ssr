6dee9cf7448a6a0d0a6d0a235f7548896b6ac1f5
‘muddyrain’ <qiu3291002845@gmail.com>
2023-05-27 21:09:41 +0800

    feat: 请求代理

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,20 @@
-import { legacy_createStore as createStore } from 'redux';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import reducer from './reducers';
-
+import thunk from 'redux-thunk';
+import { fetchMovies } from './actions/movies';
 let store;
 
 if (global.document) {
-  store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 } else {
-  store = createStore(reducer);
+  store = createStore(reducer, applyMiddleware(thunk));
+}
+
+if (global.document) {
+  window.store = store;
+  window.fetchMovies = fetchMovies;
 }
 
 export default store;
